5c22da983fcba4a0159327c6571e6c3f6d67b58b
Brian Musembi <brian.musembi@mths.ca>
2021-04-29 21:30:05 +0000

    added surface_area.cpp

---
+++ surface_area.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2021 Brian Musembi All Rights Reserved
+//
+// Created by: Brian Musembi
+// Created on: 29 April 2021
+// This program calculates the surface area of a square-based pyramid
+//     where the user gets to enter the base, height mm
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // variable declarations
+
+    int height;
+    int base;
+    int surface_area;
+
+    // input
+    std::cout << "Let's calculate the surface area of a square pyramid."
+              << std::endl;
+    std::cout << std::endl;
+    std::cout << "Enter the base length (mm): ";
+    std::cin >> base;
+    std::cout << "Enter the height (mm): ";
+    std::cin >> height;
+
+    // process
+    surface_area = (base * base) +  (base + base) * sqrt(((base * base)/4) +
+              (height * height));
+    std::cout << std::endl;
+
+    // output
+    std::cout << "Surface area is " << (surface_area) << " mm²" << std::endl;}
